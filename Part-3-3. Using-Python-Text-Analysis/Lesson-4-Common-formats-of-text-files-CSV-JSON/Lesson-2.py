f3fe53c255f3abac1d7a2abb92028b0e205f346e
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-04-02 02:33:18 +0300

    Lesson 4 of Part 3 - completed

---
+++ Part-3-3. Using-Python-Text-Analysis/Lesson-4-Common-formats-of-text-files-CSV-JSON/Lesson-2.py
@@ -0,0 +1,31 @@
+# Вам дана частичная выборка из датасета зафиксированных преступлений, совершенных в городе Чикаго с 2001 года по настоящее время.
+#
+# Одним из атрибутов преступления является его тип – Primary Type.
+#
+# Вам необходимо узнать тип преступления, которое было зафиксировано максимальное число раз в 2015 году.
+#
+# Файл с данными:
+# Crimes.csv: https://stepik.org/media/attachments/lesson/24473/Crimes.csv
+
+
+import csv
+
+tmpDict = {}
+with open('Crimes.csv') as file:
+    readerRow = csv.DictReader(file)
+    for row in readerRow:
+        if '/2015' in row['Date']:
+            typeTmp = row['Primary Type']
+            dateTmp = row['Date']
+            if row['Primary Type'] not in tmpDict:
+                tmpDict.update({typeTmp:0})
+            else:
+                count = tmpDict[typeTmp]
+                count += 1
+                tmpDict.update({typeTmp:count})
+max = 0
+for item in tmpDict:
+    if tmpDict[item] > max:
+        max = tmpDict[item]
+        rez = item
+print(rez)
